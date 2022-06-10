67f8d2a134f2009028366b9f8a32934b87c28e38
Julio <ja.samaniegom@gmail.com>
2022-06-10 11:56:53 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  
+  const [count, setCount] = useState(0)
+  const [geter, setGeter] = useState("")
+  useEffect(()=>{fetcher()},[])
+  useEffect(()=>{
+    const interval = setInterval(()=>{setCount(count+1)},1000);
+    return(()=>clearInterval(interval));
+  });
+
+  const [activityArray, setActivityArray] = useState([])
+
+  const fetcher = async ()=>{
+    const data = await fetch('https://www.boredapi.com/api/activity/')
+    const getResult= await data.json()
+    console.log(getResult)
+    setGeter(getResult.activity)
+    setActivityArray([...activityArray, getResult]) 
+  }
+
+  const incrementer = () =>{
+    setCount( count + 1)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={incrementer}>+</button>
+      <p>{count}</p>
+      <button onClick={fetcher}>activate me</button>
+      <p className="newest-activity">{geter}</p>
+      <ul>
+        {activityArray.map(activity => {
+          return (
+            <li>
+              <p key={activity.key}>{activity.activity}</p>
+              <span> Category: {activity.type}</span>
+            </li>
+          )
+        })}
+      </ul>
     </div>
   );
 }
